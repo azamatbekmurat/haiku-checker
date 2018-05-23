9152d70c4a4dc13ee1670d369ebbf975737c7967
Azamat Bekmuratov and Jim Palowski <jim@>
2018-05-23 16:46:08 -0700

    Add Diphathong function

--- spec/haiku-checker-spec.js
+++ spec/haiku-checker-spec.js
@@ -1,21 +1,27 @@
 import {HaikuCheck} from './../src/haiku-checker.js';
 
 describe ('haiku-checker', function() {
-  it ('should test whether string is haiku', function() {
-    var haiku = new HaikuCheck("selected item", "selected haikuu", "five vowels in");
-    console.log(haiku);
-    expect(haiku.checkHaiku()).toEqual("It is haiku");
-  });
+  // it ('should test whether string is haiku', function() {
+  //   var haiku = new HaikuCheck("selectd item come", "selected haikuded", "fived vowels in");
+  //   console.log(haiku);
+  //   expect(haiku.checkHaiku()).toEqual("It is haiku");
+  // });
+  //
+  // it ('should test whether string is not haiku', function() {
+  //   var haiku = new HaikuCheck("selectd item", "haikiud", "vowels five");
+  //   console.log(haiku);
+  //   expect(haiku.checkHaiku()).toEqual("Not haiku");
+  // });
+  // it ('should test whether removes silent vowel', function() {
+  //   var haiku = new HaikuCheck();
+  //   expect(haiku.removeSilentVowels("selected item come")).toEqual("selected item com");
+  //   console.log("new " + haiku.removeSilentVowels("selected item come"));
+  // });
 
-  it ('should test whether string is not haiku', function() {
-    var haiku = new HaikuCheck("selectd item", "haikiu", "vowels");
-    console.log(haiku);
-    expect(haiku.checkHaiku()).toEqual("Not haiku");
-  });
-  it ('should test whether removes silent vowel', function() {
+  it ('should test whether removes dipthong', function() {
     var haiku = new HaikuCheck();
-    expect(haiku.removeSilentVowels("selectd item come")).toEqual("selectd item com");
-    console.log("new " + haiku.removeSilentVowels("selectd item come"));
+    expect(haiku.removeDiphthong("about me haikuu")).toEqual("abut me haku");
+    console.log("new " + haiku.removeDiphthong("about me haikuu"));
   });
 
 });
--- src/haiku-checker.js
+++ src/haiku-checker.js
@@ -7,7 +7,7 @@ export class HaikuCheck {
   }
 
 
-  removeSilentVowels(row) {
+  removeDiphthong(row) {
     var vowelss = ['a', 'e', 'i', 'o', 'u'];
 
     var words = row.split(/\s+/);
@@ -18,13 +18,14 @@ export class HaikuCheck {
       var numberOfLetters = word.length;
 
       if (numberOfLetters > 2) {
-               vowelss.forEach(function(vowel) {
-                    if (word[numberOfLetters-1] == vowel) {
-                      word = word.replace(word[numberOfLetters-1], '');
-                    }
-              })
-              outputRow.push(word);
-
+        for (var i = 0; i<numberOfLetters; i++) {
+          vowelss.forEach(function(vowel) {
+               if ((word.charAt(i) == vowel) && (word.charAt(i+1) == vowel)) {
+                 word = word.replace(word.charAt(i), '');
+               }
+         })
+        }
+         outputRow.push(word);
       } else {
         outputRow.push(word);
       }
@@ -34,51 +35,86 @@ export class HaikuCheck {
     return outputString;
   }
 
-
-  checkHaiku() {
-
-    var positive = "It is haiku";
-    var negative = "Not haiku";
-    var vowels = ['a', 'e', 'i', 'o', 'u'];
-    // var consonats = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
-    // var arrRowOne = [];
-    var counterOne = 0;
-    var counterTwo = 0;
-    var counterThree = 0;
-
-    for (var i = 0; i < this.rowOne.length; i++) {
-      let currentObject = this;
-      vowels.forEach(function(vowel) {
-        if (currentObject.rowOne[i] == vowel) {
-
-          counterOne++;
-        }
-      });
-    }
-
-    for (var j = 0; j < this.rowTwo.length; j++) {
-      let currentObject = this;
-
-      vowels.forEach(function(vowel) {
-        if (currentObject.rowTwo[j] == vowel) {
-          counterTwo++;
-        }
-      });
-    }
-
-    for (var k = 0; k < this.rowThree.length; k++) {
-      let currentObject = this;
-      vowels.forEach(function(vowel) {
-        if (currentObject.rowThree[k] == vowel) {
-          counterThree++;
-        }
-      });
-    }
-
-    if ((counterOne == 5) && (counterTwo==7) && (counterThree==5)) {
-      return positive;
-    } else {
-      return negative;
-    }
-  }
+  //
+  // removeSilentVowels(row) {
+  //   var vowelss = ['a', 'e', 'i', 'o', 'u'];
+  //
+  //   var words = row.split(/\s+/);
+  //
+  //   var outputRow = [];
+  //
+  //   words.forEach(function(word) {
+  //     var numberOfLetters = word.length;
+  //
+  //     if (numberOfLetters > 2) {
+  //              vowelss.forEach(function(vowel) {
+  //                   if (word[numberOfLetters-1] == vowel) {
+  //                     word = word.replace(word[numberOfLetters-1], '');
+  //                   }
+  //             })
+  //             outputRow.push(word);
+  //
+  //     } else {
+  //       outputRow.push(word);
+  //     }
+  //
+  //   })
+  //   var outputString = outputRow.join(" ");
+  //   return outputString;
+  // }
+  //
+  //
+  // checkHaiku() {
+  //
+  //   var positive = "It is haiku";
+  //   var negative = "Not haiku";
+  //   var vowels = ['a', 'e', 'i', 'o', 'u'];
+  //   // var consonats = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+  //   // var arrRowOne = [];
+  //   var counterOne = 0;
+  //   var counterTwo = 0;
+  //   var counterThree = 0;
+  //
+  //   var rowOneWithoutSilentVowels = this.removeSilentVowels(this.rowOne);
+  //   var rowTwoWithoutSilentVowels = this.removeSilentVowels(this.rowTwo);
+  //   var rowThreeWithoutSilentVowels = this.removeSilentVowels(this.rowThree);
+  //
+  //   for (var i = 0; i < rowOneWithoutSilentVowels.length; i++) {
+  //     // let currentObject = this;
+  //     vowels.forEach(function(vowel) {
+  //       if (rowOneWithoutSilentVowels[i] == vowel) {
+  //
+  //         counterOne++;
+  //       }
+  //     });
+  //   }
+  //
+  //   for (var j = 0; j < rowTwoWithoutSilentVowels.length; j++) {
+  //     // let currentObject = this;
+  //
+  //     vowels.forEach(function(vowel) {
+  //       if (rowTwoWithoutSilentVowels[j] == vowel) {
+  //         counterTwo++;
+  //       }
+  //     });
+  //   }
+  //
+  //   for (var k = 0; k < rowThreeWithoutSilentVowels.length; k++) {
+  //     // let currentObject = this;
+  //     vowels.forEach(function(vowel) {
+  //       if (rowThreeWithoutSilentVowels[k] == vowel) {
+  //         counterThree++;
+  //       }
+  //     });
+  //   }
+  //   console.log("counter one is " + counterOne);
+  //   console.log("counter two is " + counterTwo);
+  //   console.log("counter three is " + counterThree);
+  //
+  //   if ((counterOne == 5) && (counterTwo==7) && (counterThree==5)) {
+  //     return positive;
+  //   } else {
+  //     return negative;
+  //   }
+  // }
 }
