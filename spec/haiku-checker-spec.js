bd6328ad8e40e85b9d5369dc6b9b696238f77213
Azamat Bekmuratov and Jim Palowski <azamat@>
2018-05-24 11:01:33 -0700

    Add jqeury front-end logic

--- spec/haiku-checker-spec.js
+++ spec/haiku-checker-spec.js
@@ -1,27 +1,35 @@
 import {HaikuCheck} from './../src/haiku-checker.js';
 
 describe ('haiku-checker', function() {
-  // it ('should test whether string is haiku', function() {
-  //   var haiku = new HaikuCheck("selectd item come", "selected haikuded", "fived vowels in");
-  //   console.log(haiku);
-  //   expect(haiku.checkHaiku()).toEqual("It is haiku");
-  // });
-  //
-  // it ('should test whether string is not haiku', function() {
-  //   var haiku = new HaikuCheck("selectd item", "haikiud", "vowels five");
-  //   console.log(haiku);
-  //   expect(haiku.checkHaiku()).toEqual("Not haiku");
-  // });
+  it ('should test whether string is haiku', function() {
+    var haiku = new HaikuCheck("selectd item come", "selected haikudeded", "fived vaowels ine");
+    console.log(haiku);
+    expect(haiku.checkHaiku()).toEqual("It is haiku");
+  });
+
+  it ('should test whether string is not haiku', function() {
+    var haiku = new HaikuCheck("selectd item", "haikiud", "vowels five");
+    console.log(haiku);
+    expect(haiku.checkHaiku()).toEqual("Not haiku");
+  });
+
+  //checking removeSilentVowels function
   // it ('should test whether removes silent vowel', function() {
   //   var haiku = new HaikuCheck();
   //   expect(haiku.removeSilentVowels("selected item come")).toEqual("selected item com");
   //   console.log("new " + haiku.removeSilentVowels("selected item come"));
   // });
-
-  it ('should test whether removes dipthong', function() {
+  //
+  //checking removeDiphthong function
+  // it ('should test whether removes dipthong', function() {
+  //   var haiku = new HaikuCheck();
+  //   expect(haiku.removeDiphthong("about me haikuu")).toEqual("abut me hiku");
+  //   console.log("new " + haiku.removeDiphthong("about wht is going on haikuu"));
+  // });
+  it ('should test whether removes dipthong and silent vowels', function() {
     var haiku = new HaikuCheck();
-    expect(haiku.removeDiphthong("about me haikuu")).toEqual("abut me hiku");
-    console.log("new " + haiku.removeDiphthong("about wht is going on haikuu"));
+    expect(haiku.removeSilentAndDiphthong("about me haikuue")).toEqual("abut me hiku");
+    console.log("new " + haiku.removeSilentAndDiphthong("about me haikuue"));
   });
 
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+import $ from 'jquery';
 import 'bootstrap';
+import './styles.css';
+import { HaikuCheck } from './../src/haiku-checker.js';
+
+$(document).ready(function() {
+  $('#haiku').submit(function(event) {
+    event.preventDefault();
+    var rowOne = $('#rowOne').val();
+    var rowTwo = $('#rowTwo').val();
+    var rowThree = $('#rowThree').val();
+    var output = new HaikuCheck(rowOne, rowTwo, rowThree);
+    output.checkHaiku();
+
+    $('ul#solution').append("<li>" + output.checkHaiku() + "</li>");
+
+  });
+});
